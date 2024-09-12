2918437df180513299a428012f39adc7a5092596
Vivek Gupta <vkg1257@gmai.com>
2024-09-12 18:06:11 +0530

    rename mul_value to multiple_value and add lambda information

--- multiple_value.py
+++ multiple_value.py
@@ -2,5 +2,8 @@ def mul_Value(N1, N2):
     sum = N1 + N2
     product = N1 *N2
     return sum, product
+
+# Lambda function only returns one value
+
 sum, product = mul_Value(2,5)
 print(f'Sum of values is:{sum} and product is: {product}')
