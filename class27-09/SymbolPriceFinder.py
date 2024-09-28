0811c95d776c7a866d822261b7e41a9c29413579
Vivek Gupta <vkg1257@gmai.com>
2024-09-28 16:15:22 +0530

    Updated SymbolPriceFinder Solution

--- class27-09/SymbolPriceFinder.py
+++ class27-09/SymbolPriceFinder.py
@@ -1,22 +1,10 @@
-# Q.2
-
-# stock_prices = {'AAPL': 150, 'GOOGL': 2750, 'MSFT': 300}
-# symbol = input(str("Enter your symbol: "))
-# price = stock_prices.get(symbol, "Not Found")
-# print(price)
 stocks = {'AAPL': 150, 'GOOGL': 2750, 'MSFT': 300}
 
 
 def FindPrice(symbol):
     symbol = symbol.upper()
-    for i in stocks:
-        if symbol == i:
-            i = stocks.get(symbol)
-            return i
-        else:
-            str = "Stock not Found"
-    return str
-
+    return stocks.get(symbol, 'Stock not found')
+   
 
 input = str(input('Enter Stock name: '))
 print(FindPrice(input))
