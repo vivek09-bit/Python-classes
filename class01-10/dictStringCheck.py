d8bdae0bdc867b3a181ccfecccedb962a9c849cd
Vivek Gupta <vkg1257@gmai.com>
2024-10-01 17:20:02 +0530

    Add DictStringChecker

---
+++ class01-10/dictStringCheck.py
@@ -0,0 +1,8 @@
+# names = {1:'Mukesh', 2: 'Mitesh', 3:'Komal'}
+# names = {'Mukesh', 'komal', "Mitesh"}
+# FilterNames = list(filter(lambda name:'M' in name, names))
+# print(FilterNames)
+
+# names = {1:'Mukesh', 2: 'Mitesh', 3:'Komal'}
+# FilterNames = list(filter(lambda name:name.startswith('M'), names.values()))
+# print(FilterNames)
